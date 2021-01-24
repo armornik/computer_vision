3fe7795b0ad4bfb248c91a5c7e62e374aa9bf21d
armornik <kd200681@mail.ru>
2021-01-24 22:58:49 +0300

    use pca

---
+++ use_pca.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from pylab import *
+from pca import pca
+from imtools import get_image_list
+
+im_list = get_image_list("a_thumbs")
+print(im_list[0])
+
+im = array(Image.open((im_list[0])))  # открыть одно изображение для получения размера
+
+m, n = im.shape[0:2]  # Получить размер изображения
+im_nbr = len(im_list)  # Получить число изображений
+
+# Создать матрицу для хранения всех линеаризованных изображений
+im_matrix = array([array(Image.open(im)).flatten() for im in im_list], 'f')
+
+# Выполнить метод главных компонент
+V, S, im_mean = pca(im_matrix)
+
+# Показать несколько изображений (среднее и первые 7 мод)
+figure()
+gray()
+subplot(2, 4, 1)
+imshow(im_mean.reshape(m, n))
+for i in range(7):
+    subplot(2, 4, i + 2)
+    imshow(V[i].reshape(m, n))
+
+show()
